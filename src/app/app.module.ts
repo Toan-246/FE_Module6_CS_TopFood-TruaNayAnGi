21bd1aa1455ad20451b0152b266155c6ea19eabd
LuytBui <luytbq43@gmail.com>
2022-05-11 10:30:38 +0700

    tao khung

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AdminModule} from './admin/admin.module';
+import {CustomerModule} from './customer/customer.module';
+import {MerchantModule} from './merchant/merchant.module';
+import {TestModuleModule} from './test-module/test-module.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminModule,
+    CustomerModule,
+    MerchantModule,
+    TestModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
