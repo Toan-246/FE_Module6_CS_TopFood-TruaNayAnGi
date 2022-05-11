6f6d8c8e3a9ede1a933232229ce80693a2793c5f
Toan-246 <n.v.toan687@gmail.com>
2022-05-11 16:21:50 +0700

    tao component login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './auth/login/login.component';
+import {RegisterComponent} from './auth/register/register.component';
 
 
 const routes: Routes = [
@@ -23,6 +25,14 @@ const routes: Routes = [
   {
     path: 'test',
     loadChildren: () => import('./test-module/test-module.module').then(module => module.TestModuleModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./auth/auth.module').then(module => module.AuthModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./auth/auth.module').then(module => module.AuthModule)
   }
 ];
 
