eccb333986c267e821c49aa0aebffd1edf0910c3
truonghungcuong <truonghungcuong12012001@gmail.com>
2022-05-12 09:05:32 +0700

    sua merchant-list trong admin

--- src/app/admin/merchant/merchant.module.ts
+++ src/app/admin/merchant/merchant.module.ts
@@ -2,14 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {MerchantRoutingModule} from './merchant-routing.module';
-import {MerchantComponent} from './merchant.component';
 import {MerchantListComponent} from './merchant-list/merchant-list.component';
 import {SharedModule} from '../../shared/shared.module';
 import {CustomerModule} from '../../customer/customer.module';
 
 
 @NgModule({
-  declarations: [MerchantComponent, MerchantListComponent],
+  declarations: [MerchantListComponent],
 
   imports: [
     SharedModule,
