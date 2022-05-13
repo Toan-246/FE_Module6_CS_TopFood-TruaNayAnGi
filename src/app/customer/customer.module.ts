12508bf1b9ccecc9450f08e06612f5e9acff66c4
LuytBui <luytbq43@gmail.com>
2022-05-13 09:58:52 +0700

    them homepage footer

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -4,7 +4,6 @@ import {CommonModule} from '@angular/common';
 import {CustomerRoutingModule} from './customer-routing.module';
 import {CustomerComponent} from './customer.component';
 import {FooterComponent} from './shared/footer/footer.component';
-import {NavbarComponent} from './shared/navbar/navbar.component';
 import {SharedModule} from '../shared/shared.module';
 import {MerchantRegisterComponent} from './merchant-register/merchant-register.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
