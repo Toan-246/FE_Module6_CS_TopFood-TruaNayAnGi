7bd448c24a85a694fa065bbf80327a229dea1f9d
LuytBui <luytbq43@gmail.com>
2022-05-13 10:21:00 +0700

    them link dang xuat/ dang nhap

--- src/app/shared/navbar-customer/navbar-customer.component.ts
+++ src/app/shared/navbar-customer/navbar-customer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../service/auth/auth.service';
 
 @Component({
   selector: 'app-navbar-customer',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarCustomerComponent implements OnInit {
 
-  constructor() { }
+  user: any;
+  loggedIn: boolean;
 
-  ngOnInit() {
+  constructor(private authService: AuthService) {
   }
 
+  ngOnInit() {
+    this.getCurrentUser();
+  }
+
+  getCurrentUser() {
+    const currentUser = JSON.parse(sessionStorage.getItem('user'));
+    let userId;
+    if (currentUser) {
+      userId = currentUser.id;
+    }
+
+    this.loggedIn = !!(currentUser && userId);
+  }
+
+  logout(){
+    this.authService.logout();
+    this.getCurrentUser();
+  }
 }
