21a557801f332b1f8d6f6cd10ae34e1a5f009e91
ruukichi <ngovietlc93@gmail.com>
2022-05-15 15:55:24 +0700

    edit information  merchant

--- src/app/service/use/use.service.ts
+++ src/app/service/use/use.service.ts
@@ -3,6 +3,7 @@ import {environment} from '../../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {User} from '../../model/user';
 import {Observable} from 'rxjs';
+import {Merchant} from '../../model/merchant';
 
 const API_URL = `${environment.apiUrl}`;
 
@@ -18,11 +19,15 @@ export class UseService {
     return this.http.post<User>(API_URL + '/register', user);
   }
 
-  viewUserInfo (id: number): Observable<User>{
-    return this.http.get<User>(`${API_URL}/users/${id}`)
+  viewUserInfo(id: number): Observable<User> {
+    return this.http.get<User>(`${API_URL}/users/${id}`);
   }
 
   updateUser(id: number, user): Observable<User> {
     return this.http.post<User>(`${API_URL}/users/${id}`, user);
+  }
+
+  viewMerchantInfo(userId: number): Observable<Merchant> {
+    return this.http.get(`${API_URL}/users/${userId}/merchant`);
   }
 }
