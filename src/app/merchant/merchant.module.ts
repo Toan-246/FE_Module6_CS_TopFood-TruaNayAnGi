120dc37dd28444354afcd3528ef375a77d50e269
LuytBui <luytbq43@gmail.com>
2022-05-16 16:44:45 +0700

    sua lai duong dan /merchant

--- src/app/merchant/merchant.module.ts
+++ src/app/merchant/merchant.module.ts
@@ -5,16 +5,14 @@ import {MerchantRoutingModule} from './merchant-routing.module';
 import {SharedModule} from '../shared/shared.module';
 import {ReactiveFormsModule} from '@angular/forms';
 import {FoodListComponent} from './food-list/food-list.component';
-import {MyRestaurantComponent} from './my-restaurant/my-restaurant.component';
 import {MyMerchantBannerComponent} from './my-merchant-banner/my-merchant-banner.component';
 import {SecondNavbarComponent} from './second-navbar/second-navbar.component';
-import {MerchantComponent} from './merchant.component';
 import {DishesComponent} from './dishes/dishes.component';
 import {InfoComponent} from './info/info.component';
 
 @NgModule({
-  declarations: [FoodListComponent, MyRestaurantComponent, MyMerchantBannerComponent,
-    SecondNavbarComponent, MerchantComponent, DishesComponent, InfoComponent],
+  declarations: [FoodListComponent, MyMerchantBannerComponent,
+    SecondNavbarComponent, DishesComponent, InfoComponent],
   imports: [
     SharedModule,
     CommonModule,
