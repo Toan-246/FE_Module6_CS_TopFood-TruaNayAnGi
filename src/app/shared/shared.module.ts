d51d1d53d7dbb5ad3f2590927f4ed1f8864f6daf
truonghungcuong <truonghungcuong12012001@gmail.com>
2022-05-17 15:29:38 +0700

    hoan thien avt admin/ them list shipper/ them register-shipper

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,13 +6,12 @@ import {RouterModule} from '@angular/router';
 import { NavbarCustomerComponent } from './navbar-customer/navbar-customer.component';
 import { SidebarMerchantComponent } from './sidebar-merchant/sidebar-merchant.component';
 import { SidebarUserComponent } from './sidebar-user/sidebar-user.component';
-import { NavbarMerchantRegisterComponent } from './navbar-merchant-register/navbar-merchant-register.component';
-import { NavbarShipperComponent } from './navbar-shipper/navbar-shipper.component';
+
 
 
 
 @NgModule({
-  declarations: [NavbarAdminComponent, SidebarAdminComponent, NavbarCustomerComponent, SidebarMerchantComponent, SidebarUserComponent, NavbarShipperComponent, NavbarMerchantRegisterComponent],
+  declarations: [NavbarAdminComponent, SidebarAdminComponent, NavbarCustomerComponent, SidebarMerchantComponent, SidebarUserComponent],
   imports: [
     CommonModule,
     RouterModule
@@ -23,8 +22,7 @@ import { NavbarShipperComponent } from './navbar-shipper/navbar-shipper.componen
     NavbarCustomerComponent,
     SidebarMerchantComponent,
     SidebarUserComponent,
-    NavbarShipperComponent,
-    NavbarMerchantRegisterComponent
+
   ]
 })
 export class SharedModule { }
