8ccff0d4dac3e66455e7936e728632da547f3ea6
Toan-246 <n.v.toan687@gmail.com>
2022-05-19 22:24:58 +0700

    sua form search

--- src/app/merchant/revenue/revenue.component.ts
+++ src/app/merchant/revenue/revenue.component.ts
@@ -30,26 +30,42 @@ export class RevenueComponent implements OnInit {
 
   ngOnInit() {
     this.getMerchant();
-    this.startDate = new Date();
+    // this.resetSearchForm();
+    this.getAllOrders();
     this.endDate = new Date();
+    this.startDate = new Date();
+    this.startDate.setDate(this.startDate.getDate()-30);
+    console.log(this.startDate, this.endDate);
+    (<HTMLInputElement> document.getElementById('startDate')).value = '2022-05-22';
+    (<HTMLInputElement> document.getElementById('endDate')).value = '2022-05-22';
+
   }
+
+
+  // resetSearchForm(){
+  //   (<HTMLInputElement> document.getElementById('startDate')).value = "2000-01-01";
+  //   (<HTMLInputElement> document.getElementById('endDate')).value = '2050-01-01'
+  // }
+
   getMerchant() {
     this.merchantService.getCurrentUserMerchant().subscribe(
       merchant => this.merchant = merchant
     );
   }
 
+  getTotalRevenue(){
+
+  }
   getAllOrders(){
+    this.totalRevenue = 0;
     this.startDate = (<HTMLInputElement> document.getElementById('startDate')).value;
     this.endDate = (<HTMLInputElement> document.getElementById('endDate')).value;
-
     this.merchantService.getAllOrdersByMerchantIdInPeriod(this.merchantId, this.startDate, this.endDate).subscribe(ordersBE => {
       this.orders = ordersBE;
       for (const order of this.orders) {
         this.totalRevenue += (order.total_Fee - order.service_Fee - order.shipping_Fee)
       }
     });
-
 
     console.log(this.startDate, this.endDate);
   }
