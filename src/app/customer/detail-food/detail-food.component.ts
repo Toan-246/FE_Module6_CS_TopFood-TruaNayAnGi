d58da7ec0a3e0842a35ebd0521b339209e8b44c3
LuytBui <luytbq43@gmail.com>
2022-05-19 14:25:32 +0700

    refactor food detail

--- src/app/customer/detail-food/detail-food.component.ts
+++ src/app/customer/detail-food/detail-food.component.ts
@@ -1,8 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {Dish} from '../../model/dish';
 import {DishService} from '../../service/dish/dish.service';
-import {ActivatedRoute, ParamMap} from '@angular/router';
+import {ActivatedRoute, ActivationEnd, ParamMap, Router} from '@angular/router';
 import {Merchant} from '../../model/merchant';
+import {Coupon} from '../../model/coupon';
+import {CartService} from '../../service/cart/cart.service';
+import {NotificationService} from '../../service/notification/notification.service';
+import {AuthService} from '../../service/auth/auth.service';
+import {CouponService} from '../../service/coupon/coupon.service';
 
 @Component({
   selector: 'app-detail-food',
@@ -13,13 +18,20 @@ export class DetailFoodComponent implements OnInit {
 
   id: number;
   merchant: Merchant = {};
-  dish: Dish = {};
+  dish: Dish = {categories: [], merchant: {}};
   dishesForYou: Dish[] = [];
-
+  quantity: number;
+  coupons: Coupon[];
   countChanges = 0;
+  dishTotal: number;
 
   constructor(private dishService: DishService,
-              private activatedRoute: ActivatedRoute,
+              private cartService: CartService,
+              private notificationService: NotificationService,
+              private router: Router,
+              private authService: AuthService,
+              private couponService: CouponService,
+              private activatedRoute: ActivatedRoute
   ) {
     this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
       this.id = +paramMap.get('id');
@@ -29,6 +41,25 @@ export class DetailFoodComponent implements OnInit {
 
   ngOnInit() {
     document.getElementById('dish-image').scrollIntoView(true);
+    this.quantity = 1;
+    this.getDish();
+    this.getMerchant();
+    this.getCouponByDishId();
+  }
+
+  getCouponByDishId() {
+    this.couponService.getCouponByDishId(this.id).subscribe(
+      response => this.coupons = response as Coupon[]
+    );
+  }
+
+  getDish() {
+    this.dishService.getById(this.id).subscribe(
+      (response) => {
+        this.dish = response as Dish;
+        this.calculateDishTotal();
+      }
+    );
   }
 
   getMerchant() {
@@ -42,6 +73,52 @@ export class DetailFoodComponent implements OnInit {
 
   refreshCarts($event) {
     this.countChanges = $event;
+  }
+
+  increaseQuantity() {
+    this.quantity++;
+    this.calculateDishTotal();
+  }
+
+  decreaseQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+      this.calculateDishTotal();
+    }
+  }
+
+
+  calculateDishTotal() {
+    this.dishTotal = this.dish.price * this.quantity;
+  }
+
+  addToCart() {
+    if (!this.authService.isLoggedIn()) {
+      document.getElementById('app-navbar-customer').scrollIntoView(true);
+      this.notificationService.showErrorMessage('Hãy đăng nhập để có thể đặt món');
+      return;
+    }
+    const cartDetail = {
+      dish: this.dish,
+      quantity: this.quantity
+    };
+    this.cartService.addDishToCart(cartDetail).subscribe(
+      {
+        next: (response) => {
+          this.notificationService.showSuccessMessage(`Đã thêm vào giỏ hàng </br> (${this.dish.name}) x ${this.quantity}`);
+        },
+        error: (error) => {
+          this.notificationService.showErrorMessage('Có lỗi xảy ra!');
+        },
+        complete: () => {
+          this.quantity = 1;
+          this.dishTotal = this.dish.price;
+          this.countChanges++;
+          // this.refreshNum++;
+          // this.refreshCarts.emit(this.refreshNum);
+        }
+      }
+    );
   }
 
 }
